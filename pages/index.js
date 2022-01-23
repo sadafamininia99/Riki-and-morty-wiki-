b5d9e21fb686f7df480b1d24ef895a8a392cd51e
sadafamininia99 <aminicomputer96@gmail.com>
2022-01-23 10:22:23 +0330

    title and sth

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from "next/head";
 
-
 const defaultEndpoint = `https://rickandmortyapi.com/api/character/`;
 
 export async function getServerSideProps() {
-  const res = await fetch(defaultEndpoint)
+  const res = await fetch(defaultEndpoint);
   const data = await res.json();
   return {
     props: {
-      data
-    }
-  }
+      data,
+    },
+  };
 }
 
 export default function Home({ data }) {
@@ -18,48 +17,21 @@ export default function Home({ data }) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title> Wubba Lunna Dub dub !!!!! </title>
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Wubba Lunna Dub dub !!!!!</h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <p className="description">Riki and Morty Wiki</p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className="grid">
+          <li className="card ">
+            <a href="https://nextjs.org/docs">
+              <h3>My Character</h3>
+            </a>
+          </li>
+        </ul>
       </main>
 
       <footer>
@@ -156,9 +128,11 @@ export default function Home({ data }) {
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
-
           max-width: 800px;
           margin-top: 3rem;
+          list-style: none;
+          margin-left: 0;
+          padding-left: 0;
         }
 
         .card {
